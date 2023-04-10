d7a14feaee05edce2dc8663a1dd9e63e27d1e7f2
GabCheeseBread <127157972+GabCheeseBread@users.noreply.github.com>
2023-04-10 19:10:03 -0300

    Trocada mensagem

--- saveMessage_example.py
+++ saveMessage_example.py
@@ -3,8 +3,8 @@ from cryptographyFramework import *
 initializeWrite()
 user = "Fulano"
 password = "1234"
-encryptedText = encryptMessage(user, password, "Minha mensagem secreta!")
+encryptedText = encryptMessage(user, password, "magrão = kanye west")
 saveNewLine(encryptedText)
-encryptedText = encryptMessage(user, password, "Minha segunda mensagem secreta!")
+encryptedText = encryptMessage(user, password, "kanye west = magroncio")
 saveNewLine(encryptedText)
 
